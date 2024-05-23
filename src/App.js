68fac6838fa9592575035691e8a912f0fd85b1b5
aarushroy <royaarush.07@gmail.com>
2024-05-23 08:30:55 +0530

    Project Section: Revamp

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import HeroSection from './Components/HeroSection/HeroSection'; 
 import Projects from './Components/Projects/Projects';
 import Work from './Components/Work/Work';
+import Footer from './Components/Footer/Footer'
 
 function App() {
   return (
     <div>
       <HeroSection />
       <Projects />
-      <Work />
+
     </div>
   );
 }
--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -7,15 +7,28 @@ const Projects = () => {
   return (
     <div className="projects">
       <img src={arrowproject} alt="Arrow" className="arrow-project" />
-      <h1>My Projects</h1>
-      <h2>Web Design, Frontend, Full Stack</h2>
-      <div className='projectBox'>
-        <div className='topProject'>
-          <img src={snuxplore} alt="SNUxplore" className="snuxplore-img" />
-          <div className='rightProject'>
-            <h3>SNUxplore 3.0</h3>
-            <p className='projDesc'>Lorem Ipsum hahahahahh hahah haha hahah haha haha haha</p>
+      <div className='projTop'>
+        <div className='projTopLeft'>
+          <h1>My Projects</h1>
+          <h2>Web Design, Frontend, Full Stack</h2>
+          <div className='previewButton'>
+            <h4 className='previewText'>Check them out below</h4>
           </div>
+        </div>
+        <div className='projTopRight'>
+          <div className='previewBox'></div>
+          <div className='previewBox'></div>
+          <div className='previewBox'></div>
+          <div className='previewBox'></div>
+        </div>
+      </div>
+      <div className='projectBox'>
+        <div className='leftProjectBox'>
+          <img src={snuxplore} alt="SNUxplore" className="project-img" />
+        </div>
+        <div className='rightProjectBox'>
+          <h3>SNUxplore 3.0</h3>
+          <p className='projDesc'>Lorem Ipsum hahahahahh hahah haha hahah haha hLorem Ipsum hahahahahh hah ha hahah haha hLorem Ipsum </p>
         </div>
       </div>
     </div>
